c4740a0be3dca0ba95f4a976e67e996c0d7fa6ca
scharczynski <scharcz@brandeis.edu>
2013-06-27 15:24:24 -0400

    bounce

--- src/components.js
+++ src/components.js
@@ -143,12 +143,20 @@ hurt:function() {
 },
 
 // Stops the movement
-stopMovement: function() {
-this._speed = 0;
-if (this._movement) {
-	this.x -= this._movement.x;
-	this.y -= this._movement.y;
-}
+stopMovement: function () {
+    if (this._movement) {
+        this.x -= this._movement.x;
+        if (this.hit('Solid')) {
+            this.x += this._movement.x;
+            this.y -= this._movement.y;
+            if (this.hit('Solid')) {
+                this.x -= this._movement.x;
+                this.y -= this._movement.y;
+            }
+        }
+    } else {
+        this._speed = 0;
+    }
 },
 
 
@@ -321,6 +329,8 @@ HUD_HP = function () {
 	var hp_text = Crafty.e('2D, DOM, Color, Text')
 	hp_text.attr({x:20,y:230,alpha:1.0})
 	hp_text.text('HP')
+	
+	
 }
 
     
